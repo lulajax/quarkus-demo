07a45dc8b9af95246340e95254c54c375f8f49ab
lujunjie <junjie.lu@showself.com>
2023-09-01 14:19:06 +0800

    import log4j2

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,30 +1,34 @@
 package org.acme;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.acme.data.WxArtwordTag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 @Path("/hello")
 public class GreetingResource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
 
     @Autowired
     private WxArtwordTagService artwordTagService;
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        artwordTagService.list();
         return "Hello from RESTEasy Reactive";
     }
 
     @GET
     @Path("/tag/list")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<WxArtwordTag> tagList() {
-        return artwordTagService.list();
+    public String tagList() throws JsonProcessingException {
+        LOGGER.info("tagList");
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(artwordTagService.list());
     }
 }
